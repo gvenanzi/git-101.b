2b05eaee3d9cc4ad66cba3a6b9ef4cd73fed293f
Lorenzo Di Stefano <lorenzodistefano.k@gmail.com>
2019-03-22 19:24:17 +0100

    Add py main function

--- humansizd.py
+++ humansizd.py
@@ -1,18 +1,26 @@
-SUFFIXES = [ 'KB', 'MB', 'TB', 'PB', 'EB', 'ZB', 'YB' ]
+SUFFIXES = ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
 
-def approximate_size(size):
-    """ Conver a file size to a human-readable form.
+
+def approximate_size(size=100_000):
+    """Convert a file size to human-readable form.
     Keyword arguments:
     size -- file size in bytes
-
     Returns: string
     """
     if size < 0:
-        raise ValueError('number must be non-positive')
+        raise ValueError('number must be non-negative')
+
     multiple = 1024
     for suffix in SUFFIXES:
         size /= SUFFIXES[multiple]
         if size < multiple:
             return f'{size} {suffix}'
-    raise ValueError('number too long')
-            
+    raise ValueError('number too large')
+
+
+if __name__ == '__main__':
+    print(approximate_size(1000))
+    print(approximate_size(100))
+    print(approximate_size(999000))
+    print(approximate_size(1000))
+    print(approximate_size())
